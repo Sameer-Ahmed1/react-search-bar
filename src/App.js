0876a413dcbe13a3cd914a6452c87ede37d74e1d
sameer <sameernadeemi1111@gmail.com>
2023-03-31 06:50:31 +0530

    add sort functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { Table, Select } from "@mantine/core";
+import { orderExample } from "./order";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [orders, setOrders] = useState([]);
+  const [sortCriteria, setSortCriteria] = useState("");
+  const [sortOrder, setSortOrder] = useState("");
+  useEffect(() => {
+    setOrders(orderExample);
+  }, []);
+  const sortedOrders =
+    sortCriteria && sortOrder
+      ? orders.sort((a, b) => {
+          if (!sortCriteria || !sortOrder) return 0;
+          if (a[sortCriteria] < b[sortCriteria]) {
+            return sortOrder === "ascending" ? -1 : 1;
+          } else if (a[sortCriteria] > b[sortCriteria]) {
+            return sortOrder === "ascending" ? 1 : -1;
+          } else {
+            return 0;
+          }
+        })
+      : orders;
+
+  const rows = sortedOrders.map((order, index) => (
+    <tr key={`${index}`}>
+      <td>{order.user}</td>
+      <td>{order.shipper}</td>
+      <td>{order.weight}</td>
+      <td>{order.cost}</td>
+      <td>{order.source}</td>
+      <td>{order.destination}</td>
+      <td>{order.status}</td>
+    </tr>
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Select
+        label="Sort By"
+        placeholder="Pick one"
+        data={[
+          { value: "cost", label: "Cost" },
+          { value: "weight", label: "Weight" },
+        ]}
+        value={sortCriteria}
+        onChange={setSortCriteria}
+      />
+      <Select
+        placeholder="Pick one"
+        data={[
+          { value: "ascending", label: "Ascending" },
+          { value: "descending", label: "Descending" },
+        ]}
+        value={sortOrder}
+        onChange={setSortOrder}
+      />
+      <Table withBorder highlightOnHover>
+        <thead>
+          <tr>
+            <th>User</th>
+            <th>Shipper</th>
+            <th>Weight</th>
+            <th>Cost</th>
+            <th>Source</th>
+            <th>Destination</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </Table>
     </div>
   );
-}
-
+};
 export default App;
